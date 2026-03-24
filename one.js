8145374ba6d6d1a42958fe26ce76f71b8f6654ff
Kritii <kritikumari53782@gmail.com>
2026-03-25 01:54:16 +0530

    js..

--- one.js
+++ one.js
@@ -2,3 +2,33 @@ const inputBox=document.getElementById("input-box");
 const listContainer=document.getElementById("list-container");
 const deadlineInput = document.getElementById("deadline");
 
+function addTask(){
+    if(inputBox.value===''){
+        alert("You must write something!");
+    }
+    else{
+        let li=document.createElement("li");
+        let taskText = inputBox.value;
+        let deadline = deadlineInput.value;
+        let timeLeft = "";
+        if(deadline){
+            let diff = new Date(deadline) - new Date();
+            let hours = Math.floor(diff / (1000 * 60 * 60));
+            if(hours < 0){
+                timeLeft = " (Overdue)";
+            } else {
+                timeLeft = ` (${hours} hrs left)`;
+            }
+        }
+
+        li.innerHTML = taskText + timeLeft;
+        listContainer.prepend(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+
+    inputBox.value= "";
+    deadlineInput.value = "";
+    saveData();
+}
