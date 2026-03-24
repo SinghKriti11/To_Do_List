f3594836a6f3ccedee564eb0b28c95032adeb7b1
Kritii <kritikumari53782@gmail.com>
2026-03-25 01:51:58 +0530

    js

--- one.js
+++ one.js
@@ -2,7 +2,3 @@ const inputBox=document.getElementById("input-box");
 const listContainer=document.getElementById("list-container");
 const deadlineInput = document.getElementById("deadline");
 
-function addTask(){
-    if(inputBox.value===''){
-        alert("You must write something!");
-    }
